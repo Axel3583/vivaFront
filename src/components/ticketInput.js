7935f2c6f672f7389f554af15e7ac085317522a1
Armel Steve OUETCHOUA <armel.ouetchoua@tilkal.com>
2023-05-23 15:17:56 +0200

    Mise en place de la page scanne screen

---
+++ src/components/ticketInput.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+
+const TicketInput = ({
+  ticketCode,
+  handleTicketCodeChange,
+  handleTicketCodeSubmit,
+  handleGoBack,
+}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleTicketCodeChange}
+        value={ticketCode}
+        placeholder="Enter ticket code"
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleTicketCodeSubmit}
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleGoBack}>
+          <Text style={styles.buttonText}>Retour</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+    width: "80%",
+    fontSize: 16,
+    backgroundColor: "#fff",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    maxWidth: "90%",
+  },
+  button: {
+    backgroundColor: "#3498db",
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+export default TicketInput;
