583ed4ecb19e05fccb4507344cf2a41982132632
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2023-05-25 17:13:38 +0200

    columns adjusted with btns

--- src/screens/programScreen.js
+++ src/screens/programScreen.js
@@ -2,47 +2,126 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
 
 export default function ProgramScreen() {
-    const [columnsLayout, setColumnsLayout] = useState("A");
+    const [selectedButton, setSelectedButton] = useState("Wednesday 14");
 
     const handleLogoPress = () => {
         navigation.navigate('Home');
     };
 
-    const handleSwitchLayout = () => {
-        setColumnsLayout(columnsLayout === "A" ? "B" : "A");
+    const handleButtonPress = (button) => {
+        setSelectedButton(button);
     };
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={handleLogoPress}>
-                <Image
-                    source={require('../../assets/image/logo-v.png')}
-                    style={styles.logo}
-                />
-            </TouchableOpacity>
+            <View style={styles.logoContainer}>
+                <TouchableOpacity onPress={handleLogoPress}>
+                    <Image
+                        source={require('../../assets/image/logo-v.png')}
+                        style={styles.logo}
+                    />
+                </TouchableOpacity>
+            </View>
             <View style={styles.buttonContainer}>
-                <TouchableOpacity onPress={handleSwitchLayout} style={styles.button}>
-                    <Text style={styles.buttonText}>Switch Layout</Text>
+                <TouchableOpacity
+                    onPress={() => handleButtonPress("Wednesday 14")}
+                    style={[
+                        styles.button,
+                        selectedButton === "Wednesday 14" && styles.selectedButton,
+                    ]}
+                >
+                    <Text style={styles.buttonText}>Wednesday 14</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => handleButtonPress("Thursday 15")}
+                    style={[
+                        styles.button,
+                        selectedButton === "Thursday 15" && styles.selectedButton,
+                    ]}
+                >
+                    <Text style={styles.buttonText}>Thursday 15</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => handleButtonPress("Friday 16")}
+                    style={[
+                        styles.button,
+                        selectedButton === "Friday 16" && styles.selectedButton,
+                    ]}
+                >
+                    <Text style={styles.buttonText}>Friday 16</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => handleButtonPress("Saturday 17")}
+                    style={[
+                        styles.button,
+                        selectedButton === "Saturday 17" && styles.selectedButton,
+                    ]}
+                >
+                    <Text style={styles.buttonText}>Saturday 17</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.columnsContainer}>
-                {columnsLayout === "A" && (
+                {selectedButton === "Wednesday 14" && (
                     <View style={styles.column}>
                         <View style={styles.block}>
-                            <Text>Column A - Block 1</Text>
+                            <Text>Wednesday 14 - Column A - Block 1</Text>
                         </View>
                         <View style={styles.block}>
-                            <Text>Column A - Block 2</Text>
+                            <Text>Wednesday 14 - Column A - Block 2</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Wednesday 14 - Column B - Block 1</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Wednesday 14 - Column B - Block 2</Text>
                         </View>
                     </View>
                 )}
-                {columnsLayout === "B" && (
+                {selectedButton === "Thursday 15" && (
                     <View style={styles.column}>
                         <View style={styles.block}>
-                            <Text>Column B - Block 1</Text>
+                            <Text>Thursday 15 - Column A - Block 1</Text>
                         </View>
                         <View style={styles.block}>
-                            <Text>Column B - Block 2</Text>
+                            <Text>Thursday 15 - Column A - Block 2</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Thursday 15 - Column B - Block 1</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Thursday 15 - Column B - Block 2</Text>
+                        </View>
+                    </View>
+                )}
+                {selectedButton === "Friday 16" && (
+                    <View style={styles.column}>
+                        <View style={styles.block}>
+                            <Text>Friday 16 - Column A - Block 1</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Friday 16 - Column A - Block 2</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Friday 16 - Column B - Block 1</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Friday 16 - Column B - Block 2</Text>
+                        </View>
+                    </View>
+                )}
+                {selectedButton === "Saturday 17" && (
+                    <View style={styles.column}>
+                        <View style={styles.block}>
+                            <Text>Saturday 17 - Column A - Block 1</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Saturday 17 - Column A - Block 2</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Saturday 17 - Column B - Block 1</Text>
+                        </View>
+                        <View style={styles.block}>
+                            <Text>Saturday 17 - Column B - Block 2</Text>
                         </View>
                     </View>
                 )}
@@ -59,10 +138,13 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         flexDirection: "column",
     },
-    logo: {
-        position: 'absolute',
+    logoContainer: {
+        position: "absolute",
         top: 60,
         left: 20,
+        zIndex: 1,
+    },
+    logo: {
         width: 60,
         height: 60,
     },
@@ -76,6 +158,10 @@ const styles = StyleSheet.create({
         paddingVertical: 5,
         backgroundColor: "lightgray",
         borderRadius: 5,
+        marginRight: 10,
+    },
+    selectedButton: {
+        backgroundColor: "gray",
     },
     buttonText: {
         fontWeight: "bold",
@@ -92,9 +178,10 @@ const styles = StyleSheet.create({
     },
     block: {
         backgroundColor: "lightblue",
-        width: (width - 80) / 2, // Adjust the width of the blocks based on the available space
         marginVertical: 10,
         padding: 10,
         borderRadius: 5,
+        flex: 1,
+        marginHorizontal: 5,
     },
 });
