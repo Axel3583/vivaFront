d4f55c5b16d86138e3d89d681d4a404854ea3cc3
Armel Steve OUETCHOUA <armel.ouetchoua@tilkal.com>
2023-05-25 12:31:36 +0200

    correction App2

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import HomeScreen from './src/screens/homeScreen';
-import ScannerForm from './src/screens/scanneScreen';
-import AppIntro from './src/components/appIntroSlider';
-import * as Font from 'expo-font';
-import { StyleSheet, ActivityIndicator, Animated, View, Image, ImageBackground } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import HomeScreen from "./src/screens/homeScreen";
+import ScannerForm from "./src/screens/scanneScreen";
+import AppIntro from "./src/components/appIntroSlider";
+import * as Font from "expo-font";
+import {
+  StyleSheet,
+  ActivityIndicator,
+  Animated,
+  View,
+  Image,
+  ImageBackground,
+} from "react-native";
 import {
   SafeAreaProvider,
   initialWindowMetrics,
-} from 'react-native-safe-area-context';
+} from "react-native-safe-area-context";
 
 const Tab = createMaterialBottomTabNavigator();
 
-const  isValidTicket = false
-
+const isValidTicket = false;
 
 // const handleTicketCodeSubmit = () => {
 // //   const { ticketCode } = state;
@@ -32,23 +38,23 @@ function TabNavigator() {
       initialRouteName="Scanne"
       activeColor="#f55e30"
       inactiveColor="#3e2465"
-
-      barStyle={{ backgroundColor: '#FAF9F7' }} >
-
+      barStyle={{ backgroundColor: "#FAF9F7" }}
+    >
       <Tab.Screen
         options={{
-          tabBarLabel: 'Ticket',
+          tabBarLabel: "Ticket",
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="ticket" color={color} size={26} />),
+            <MaterialCommunityIcons name="ticket" color={color} size={26} />
+          ),
         }}
         name="Scanne"
         component={ScannerForm}
       />
 
-      {!misValidTicket ? (
+      {!isValidTicket ? (
         <Tab.Screen
           options={{
-            tabBarLabel: 'Home',
+            tabBarLabel: "Home",
             tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="home" color={color} size={26} />
             ),
@@ -65,15 +71,15 @@ export default class App extends React.Component {
   state = {
     showIntro: true,
     isLoading: false,
-    fontLoaded: false
+    fontLoaded: false,
   };
 
   fetchFonts = () => {
     return Font.loadAsync({
-      'MuseoSans_500': require('./assets/fonts/MuseoSans_500.otf'),
-      'MuseoSans_700': require('./assets/fonts/MuseoSans_700.otf'),
-      'MuseoSans_900': require('./assets/fonts/MuseoSans_900.otf'),
-      'MuseoSans_100': require('./assets/fonts/MuseoSans-100.otf'),
+      MuseoSans_500: require("./assets/fonts/MuseoSans_500.otf"),
+      MuseoSans_700: require("./assets/fonts/MuseoSans_700.otf"),
+      MuseoSans_900: require("./assets/fonts/MuseoSans_900.otf"),
+      MuseoSans_100: require("./assets/fonts/MuseoSans-100.otf"),
     });
   };
 
@@ -86,14 +92,13 @@ export default class App extends React.Component {
     this.setState({ fontLoaded: true });
   };
 
-
   handleDone = () => {
     this.setState({ isLoading: true });
     this.setState({ showIntro: false });
     setTimeout(() => {
       this.setState({
         showIntro: false,
-        isLoading: false
+        isLoading: false,
       });
     }, 2000);
   };
@@ -104,28 +109,27 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          {isLoading ?
-            (fontLoaded ?
-
-              <View style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: '#E47B24'
-              }}>
+          {isLoading ? (
+            fontLoaded ? (
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "#E47B24",
+                }}
+              >
                 <Image
-                  source={require('./assets/image/10.png')}
+                  source={require("./assets/image/10.png")}
                   style={{ width: 350, height: 150 }}
                 />
               </View>
-
-
-              : null)
-            : showIntro ? (
-              <AppIntro handleDone={this.handleDone} />
-            ) : (
-              <TabNavigator />
-            )}
+            ) : null
+          ) : showIntro ? (
+            <AppIntro handleDone={this.handleDone} />
+          ) : (
+            <TabNavigator />
+          )}
         </SafeAreaProvider>
       </NavigationContainer>
     );
@@ -135,16 +139,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   backgroundImage: {
     flex: 1,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
 });
-
-
